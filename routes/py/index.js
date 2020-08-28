fd43914cf15dc18221f44feb2ac59ae68b68bc15
Vsevolod Zolotov <vsevolod.zolotov02@mail.ru>
2020-08-28 23:50:13 +0700

    rewrite T to secs

--- routes/py/index.js
+++ routes/py/index.js
@@ -6,10 +6,8 @@ router.get('/', (req, res) => {
   const store = global.store
   if (store.currLamp.name && store.currLamp.type) {
     const OC = +req.query.degs * 60 + +req.query.mins
-    const T = req.query.time
-      .split(':')
-      .map(item => +item)
-      .reduce((curr, prev) => curr * prev)
+    const hms = req.query.time.split(':').map(item => +item)
+    const T = hms[0] * 3600 + hms[1] * 60 + hms[2]
     const payload = {
       OC,
       T,
