928e3b7d422616bd789bf9bbb7218968a6b3a084
Vsevolod Zolotov <vsevolod.zolotov02@mail.ru>
2020-08-30 01:34:01 +0700

    spa

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const WebSocket = require('ws')
 const http = require('http')
+const path = require('path')
 require('./store')
 
 const apiApp = express()
+const spaApp = express()
 const apiServer = http.createServer(apiApp)
+const spaServer = http.createServer(spaApp)
 const wss = new WebSocket.Server({ server: apiServer })
 
 module.exports = { wss }
@@ -23,6 +26,17 @@ apiApp.use(cors())
 apiApp.use('/py', pyRouter)
 apiApp.use('/api', apiRouter)
 
+spaApp.use(express.static(path.join(__dirname, 'dist')))
+
+spaApp.get('*', (req, res) => {
+  console.log(path.join(__dirname, '/dist/index.html'))
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
 apiServer.listen(5000, () => {
   console.log('API-server started on port 5000')
+})
+
+spaServer.listen(8080, () => {
+  console.log('SPA-server started on port 8080')
 })
